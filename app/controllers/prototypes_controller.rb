738f98c1620dd0ac68b27f5367f171cf4ef7d2b9
koseki3104 <satoshi4500sports@icloud.com>
2023-06-22 18:16:49 +0900

    プロトタイプ編集機能　コントローラー（edit）編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,20 @@
 class PrototypesController < ApplicationController
-  def index
-  end
+      def index
+      end
+    
+      def show
+        @prototype = Prototype.find(params[:id])
+      end
+  
+      def destroy
+        prototype = Prototype.find(params[:id])
+        prototype.destroy
+        redirect_to root_path
+      end
+
+      def edit
+        @prototype = Prototype.find(params[:id])
+      end
 end
+    
+  
