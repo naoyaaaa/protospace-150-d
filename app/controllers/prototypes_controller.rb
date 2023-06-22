2296a7bcc72573ebbec75f99761727d9e97b2041
naoyaaaa <23nakajima.naoya@gmail.com>
2023-06-22 16:58:24 +0900

    Active Storageの導入

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,10 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
