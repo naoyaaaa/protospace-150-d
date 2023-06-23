22b7ecc5834ff50838f83dbd161465859b0f9843
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-06-23 12:39:38 +0900

    ログインしないとトップに戻される機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, except:[:index,:show]
+
   def index
   end
 
@@ -20,4 +22,11 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+      
 end
