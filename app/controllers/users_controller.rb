f46a172673e5d62f44bfcaab7066b675c29805d2
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-06-26 15:34:47 +0900

    Revert "Merge pull request #20 from naoyaaaa/ユーザー詳細ページ機能2"
    This reverts commit 171a66b0dac093a184b40848a46b86868c1e5f83, reversing
    changes made to a1e5dbc7e562e0d47690a36b3872df440fb73009.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-
-  def show
-    @user = User.find(params[:id])
-    @prototypes = @user.prototypes
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
    devise_for :users
    root to: 'prototypes#index'
-   resources :users, only: :show
    resources :prototypes
 end
